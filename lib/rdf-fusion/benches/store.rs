6b44bc90eda4040371262ef635093936cabd5b99
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-06-06 17:44:20 +0200

    Add a benchmark for querying an empty store

--- lib/rdf-fusion/benches/store.rs
+++ lib/rdf-fusion/benches/store.rs
@@ -1,11 +1,14 @@
 #![allow(clippy::panic)]
 
 use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
+use futures::StreamExt;
 use rdf_fusion::model::Term;
 use rdf_fusion::store::Store;
+use rdf_fusion_engine::results::QueryResults;
 use rdf_fusion_model::{GraphName, NamedNode, Quad, Subject};
 use tokio::runtime::Runtime;
 
+/// This benchmark measures transactionally inserting synthetic quads into the store.
 fn store_load(c: &mut Criterion) {
     c.bench_function("Store::load", |b| {
         b.to_async(Runtime::new().unwrap()).iter(|| async {
@@ -17,8 +20,27 @@ fn store_load(c: &mut Criterion) {
     });
 }
 
-criterion_group!(store, store_load);
-criterion_main!(store);
+/// This benchmarks measure the duration of running a simple query (1 triple pattern) against an
+/// empty store. Hopefully, this can provide insights into the "baseline" overhead of the query
+/// engine.
+fn store_empty_query(c: &mut Criterion) {
+    c.bench_function("Store::query - Empty Store", |b| {
+        b.to_async(Runtime::new().unwrap()).iter(|| async {
+            let store = Store::new();
+            let result = store.query("SELECT ?s ?p ?o {  ?s ?p ?o }").await.unwrap();
+            match result {
+                QueryResults::Solutions(mut sol) => {
+                    assert!(matches!(sol.next().await, None), "No result expected");
+                }
+                _ => panic!("Unexpected QueryResults"),
+            }
+        });
+    });
+}
+
+criterion_group!(store_write, store_load);
+criterion_group!(store_query, store_empty_query);
+criterion_main!(store_write, store_query);
 
 fn generate_quads(count: u64) -> impl Iterator<Item = Quad> {
     (0..count).map(|i| {
