782240fec284d4cb44c9c00006e3ac60477f3bb2
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-07-25 13:45:35 +0200

    Delete unused fuzz

--- fuzz/fuzz_targets/sparql_query.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use spargebra::Query;
-
-fuzz_target!(|data: &str| {
-    let _ = Query::parse(data, None);
-});
--- fuzz/fuzz_targets/sparql_results_xml.rs
+++
@@ -1,6 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use oxigraph_fuzz::result_format::fuzz_result_format;
-use sparesults::QueryResultsFormat;
-
-fuzz_target!(|data: &[u8]| fuzz_result_format(QueryResultsFormat::Xml, data));
