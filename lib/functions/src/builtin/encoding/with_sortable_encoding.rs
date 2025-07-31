dbe1d4fb5813b23203e2492e15f8a933790dd6c4
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-07-31 13:24:05 +0200

    Minor hash UDF changes

--- lib/functions/src/builtin/encoding/with_sortable_encoding.rs
+++ lib/functions/src/builtin/encoding/with_sortable_encoding.rs
@@ -138,9 +138,9 @@ impl ScalarUDFImpl for WithSortableEncoding {
     }
 
     fn hash_value(&self) -> u64 {
+        // Remove once https://github.com/apache/datafusion/pull/16977 is in release
         let hasher = &mut DefaultHasher::new();
         self.as_any().type_id().hash(hasher);
-        self.name().hash(hasher);
         hasher.finish()
     }
 }
