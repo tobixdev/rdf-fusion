1f6c0bbaa3a838a710e8a5720c79af48ab3651f6
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-08-06 15:30:18 +0200

    Try sort merge joins

--- lib/execution/src/engine.rs
+++ lib/execution/src/engine.rs
@@ -65,11 +65,15 @@ impl RdfFusionContext {
         let registry: Arc<dyn RdfFusionFunctionRegistry> =
             Arc::new(DefaultRdfFusionFunctionRegistry::new(encodings.clone()));
 
+        let mut config = SessionConfig::new().with_target_partitions(1);
+        config.options_mut().optimizer.prefer_hash_join = false;
+        config.options_mut().optimizer.prefer_existing_sort = true;
+
         let state = SessionStateBuilder::new()
             .with_query_planner(Arc::new(RdfFusionPlanner::new(Arc::clone(&storage))))
             .with_aggregate_functions(vec![AggregateUDF::from(FirstValue::new()).into()])
             // TODO: For now we use only a single partition. This should be configurable.
-            .with_config(SessionConfig::new().with_target_partitions(1))
+            .with_config(config)
             .build();
 
         let session_context = SessionContext::from(state);
