eb7b2903a9a0cb8942ed997ae49fd8c05d2e0f8a
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-08-14 14:01:16 +0200

    Use file format from content-type, not accept, header

--- lib/web/src/repositories/data.rs
+++ lib/web/src/repositories/data.rs
@@ -1,18 +1,24 @@
-use crate::AppState;
 use crate::error::RdfFusionServerError;
+use crate::AppState;
 use anyhow::anyhow;
 use axum::body::Body;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::Response;
+use axum_extra::TypedHeader;
+use headers::ContentType;
 use rdf_fusion::error::LoaderError;
 use rdf_fusion::io::{RdfFormat, RdfParser};
 
 pub async fn handle_data_post(
+    content_type: TypedHeader<ContentType>,
     State(state): State<AppState>,
-    format: RdfFormat,
     body: String,
 ) -> Result<Response, RdfFusionServerError> {
+    let format =
+        RdfFormat::from_media_type(&content_type.0.to_string()).ok_or_else(|| {
+            RdfFusionServerError::BadRequest("Invalid content type.".to_owned())
+        })?;
     let parser = RdfParser::from_format(format);
 
     // TODO logging
@@ -21,9 +27,10 @@ pub async fn handle_data_post(
         .load_from_reader(parser, body.as_bytes())
         .await
         .map_err(|error| match error {
-            LoaderError::Parsing(err) => {
-                RdfFusionServerError::BadRequest(err.to_string())
-            }
+            LoaderError::Parsing(err) => RdfFusionServerError::BadRequest(format!(
+                "Error parsing {} RDF file: {}",
+                format, err
+            )),
             LoaderError::Storage(_) => {
                 RdfFusionServerError::Internal(anyhow!("Error with storage layer."))
             }
