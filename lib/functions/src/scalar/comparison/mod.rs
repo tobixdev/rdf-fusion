9c092b535dfded3b2cb3c95c9acb9dbca5644328
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-10-10 10:20:01 +0200

    Use Equality instead of SameTerm

--- lib/functions/src/scalar/comparison/mod.rs
+++ lib/functions/src/scalar/comparison/mod.rs
@@ -3,11 +3,9 @@ mod greater_or_equal;
 mod greater_than;
 mod less_or_equal;
 mod less_than;
-mod same_term;
 
 pub use equal::EqualSparqlOp;
 pub use greater_or_equal::GreaterOrEqualSparqlOp;
 pub use greater_than::GreaterThanSparqlOp;
 pub use less_or_equal::LessOrEqualSparqlOp;
 pub use less_than::LessThanSparqlOp;
-pub use same_term::SameTermSparqlOp;
