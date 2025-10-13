5489c822934ed8a53d64b6fc19e8e26d1a4df431
Tobias Schwarzinger <tobias.schwarzinger@tuwien.ac.at>
2025-10-13 10:44:16 +0200

    Use filters for asserting plans

--- bench/tests/plans/mod.rs
+++ bench/tests/plans/mod.rs
@@ -4,36 +4,19 @@
 //! - To ensure that the query plans do not change unexpectedly.
 //! - Given a new optimization, verify that the "end to end" query plans are indeed changed.
 
-use regex::Regex;
-use std::collections::HashMap;
-use std::fmt::Write;
+use insta::Settings;
 
 mod bsbm_business_intelligence;
 mod bsbm_explore;
 mod wind_farm;
 
-fn canonicalize_uuids(s: &str) -> String {
+fn run_plan_assertions(assertions: impl FnOnce()) {
+    let mut settings = Settings::default();
+
     // This is a bit hacky. Oxigraph does not print leading zeroes, and therefore we must replace
     // also shorter uuids. We assume that more than 12 leading zeroes are very unlikely for random
     // uuids and that, on the other hand, 20 characters long hex numbers are also unlikely in LPs.
-    let uuid_re = Regex::new(r"\b[0-9a-fA-F]{20,32}\b").unwrap();
+    settings.add_filter(r"\b[0-9a-fA-F]{20,32}\b", "<uuid>");
 
-    let mut uuid_map = HashMap::new();
-    let mut counter = 1u128;
-
-    let result = uuid_re.replace_all(s, |caps: &regex::Captures| {
-        let uuid = caps.get(0).unwrap().as_str();
-
-        // Map to a deterministic fake UUID if not already mapped
-        let entry = uuid_map.entry(uuid.to_string()).or_insert_with(|| {
-            let mut fake_uuid = String::new();
-            write!(&mut fake_uuid, "{:032x}", counter).unwrap();
-            counter += 1;
-            fake_uuid
-        });
-
-        entry.clone()
-    });
-
-    result.into_owned()
+    settings.bind(|| assertions());
 }
